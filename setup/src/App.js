ed0e26d45525a0ebd5e8b9b608fc52d55829b318
Murali Murugan <murali732000@gmail.com>
2023-03-01 23:23:03 +0530

    commit

--- setup/src/App.js
+++ setup/src/App.js
@@ -9,8 +9,7 @@ function App() {
     <main>
        <section>
       <Router>
-       
-        <Header />
+         <Header />
         <Routes>
           <Route exact path="/" element={<Menu />} />
         </Routes>
